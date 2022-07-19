3d94565a19886855cecdd4f65963532ede566e97
marsamogin <marsamogin@gmail.com>
2022-07-19 16:28:15 -0300

    Update bkp-all-my.sh

--- bkp-all-my.sh
+++ bkp-all-my.sh
@@ -1,11 +1,11 @@
-#!/bin/bash -xv
+#!/bin/bash
 #
 # Backup total do MYSQL
 #
 
-rm -rf /backup/mysql-all/*.gz
-cd /backup/mysql-all
+rm -rf /backup/databases/mysql_all*.gz
+cd /backup/databases
 
-/usr/bin/mysqldump --all-databases --events > mysql_all_`date +%d%m%y`.sql
+/usr/bin/mysqldump -u marcio_bkp -pI8hErcT2ww39kA3lBq01 --all-databases --events > mysql_all_`date +%d%m%y`.sql
 
 gzip *.sql
