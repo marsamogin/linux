fc66a14fc1ed97fea1875c6a9885f4972162a23c
Marcio Samogin <marsamogin@gmail.com>
2025-08-07 12:19:22 -0300

    Delete create-user.sh

--- create-user.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-useradd -d /home/keite keite
-usermod -aG wheel keite
-mkdir /home/keite/.ssh
-touch /home/keite/.ssh/authorized_keys
-chown -R keite:keite /home/keite/.ssh
-chmod 700 /home/keite/.ssh
-chmod 600 /home/keite/.ssh/authorized_keys
